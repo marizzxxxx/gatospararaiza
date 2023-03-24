dc3e874819ef4ac897192b6e5667e43d02d06888
Maria Eduarda Aires <104572741+marizzxxxx@users.noreply.github.com>
2023-03-24 19:08:02 -0300

    att 2

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ document.addEventListener("click", () => {
     })
     .catch(error => console.error(error));
 });
+
+var agora = new Date();
+var hora = agora.getHours();
+var mensagem;
+
+if (hora >= 6 && hora < 12) {
+    mensagem = "Bom dia, meu bem!";
+} else if (hora >= 12 && hora < 18) {
+    mensagem = "Boa tarde, amor!";
+} else if (hora >= 18 || hora < 23) {
+    mensagem = "Esses gatos filhotes te </br> desejam uma boa noite!";
+} else {
+    mensagem = "Não tá meio tarde para estar </br> vendo essas coisinhas feias?";
+}
+
+document.getElementById("mensagem").innerHTML = mensagem;
+
+var balao = document.getElementById("balao");
+balao.classList.remove("esconder");
+setTimeout(function() {
+  balao.classList.add("esconder");
+  balao.addEventListener("animationend", function() {
+    balao.remove();
+  });
+}, 6000); // altere o valor (em milissegundos) para mudar o tempo de exibição do balão
